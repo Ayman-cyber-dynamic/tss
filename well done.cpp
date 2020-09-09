906660b5a0746e6e0d2b46ff16387e2830788cf8
Ayman-cyber-dynamic <70891692+Ayman-cyber-dynamic@users.noreply.github.com>
2020-09-09 13:43:30 +0200

    Add files via upload

---
+++ well done.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter Your lucky Number" << endl;
+    cin >> num;
+
+    cout<< "****************\n";
+
+    cout << num*2+1;
+    return 0;
+}
